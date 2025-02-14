29f44148e98642d2db828d68e8b4cb0218cc36f6
André Aubin <andre@andral.xyz>
2025-02-14 14:39:55 +0100

    feat: preload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,25 @@
 import type { Metadata } from "next";
-import "./globals.css";
-
 import config from '@/config.json'
 import type { Viewport } from 'next'
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Inconsolata, Poppins } from 'next/font/google'
+import { PreloadResources } from "@/components/PreloadResources/PreloadResources";
+
+import "./globals.css";
+
+const inconsolata = Inconsolata({
+  subsets: ['latin'],
+  variable: '--font-inconsolata',
+  display: 'swap',
+})
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  display: 'swap',
+  weight: ['400', '700']
+})
 
 export const viewport: Viewport = {
   themeColor: '#ef6361',
@@ -12,6 +29,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(config.siteUrl),
   title: config.title,
   description: config.description,
+  
 };
 
 export default function RootLayout({
@@ -21,8 +39,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inconsolata.variable} ${poppins.variable}`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
+        <PreloadResources />
       </body>
     </html>
   );
